7f99cb46cb9945e42ee89cafedf48500c86aea88
Dhiraj Khapangi <dkhapangi13@outlook.com>
2020-06-23 16:19:55 +0545

    seed data

--- EmployeeManagement/Models/AppDbContext.cs
+++ EmployeeManagement/Models/AppDbContext.cs
@@ -15,5 +15,9 @@ namespace EmployeeManagement.Models
         }
 
         public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
     }
 }
