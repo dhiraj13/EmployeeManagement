6872fb0c4f10a903bd4070e501d404302fa0e538
Dhiraj Khapangi <dkhapangi13@outlook.com>
2020-06-27 09:24:05 +0545

    logging exceptions

--- EmployeeManagement/Controllers/ErrorController.cs
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -5,11 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger;
+        }
+
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
@@ -19,8 +27,8 @@ namespace EmployeeManagement.Controllers
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
-                    ViewBag.Path = statusCodeResult.OriginalPath;
-                    ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    logger.LogWarning($"404 Error Occured. Path = {statusCodeResult.OriginalPath}" +
+                        $"and QueryString = {statusCodeResult.OriginalQueryString}");
                     break;
             }
 
@@ -33,9 +41,9 @@ namespace EmployeeManagement.Controllers
         {
             var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
 
-            ViewBag.ExceptionPath = exceptionDetails.Path;
-            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
-            ViewBag.Stacktrace = exceptionDetails.Error.StackTrace;
+            // logger.LogError will log an exception
+            logger.LogError($"The path {exceptionDetails.Path} threw an exception " +
+                $"{exceptionDetails.Error}");
 
             return View("Error");
         }
